2d63ea04a74cad0a52647e0035176c4aef591036
Takumi19980801 <takunomi1998@gmail.com>
2024-03-28 16:12:06 +0900

    java変数

--- src/com/example/mynavi/Hello.java
+++ src/com/example/mynavi/Hello.java
@@ -1,15 +1,11 @@
 // （1）パッケージ宣言
 package com.example.mynavi;
-
-//（2）クラス宣言
+ 
+// （2）クラス宣言
 public class Hello {
-	// （3）メソッド宣言
-
-	public static void main(String[] args) {
-		// （4）文
-		final String GREETING_MSG = "こんにちは、世界！";
-		System.out.println(GREETING_MSG);
-
-	}
-
+  // （3）メソッド宣言
+  public static void main(String[] args) {
+    // （4）文
+    System.out.println("こんにちは、世界");
+  }
 }
---
+++ src/com/example/mynavi/basic/VarBasic.java
@@ -0,0 +1,8 @@
+package com.example.mynavi.basic;
+ 
+public class VarBasic {
+  public static void main(String[] args) {
+    var num = 3.14;
+    System.out.println(num);    // 結果：3.14
+  }
+}
