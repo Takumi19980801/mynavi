f7aa75ef8e10735abb8056a5fce2ca6dbac88110
Takumi19980801 <takunomi1998@gmail.com>
2024-03-28 14:23:10 +0900

    Initial commit

---
+++ src/com/example/mynavi/Hello.java
@@ -0,0 +1,15 @@
+// （1）パッケージ宣言
+package com.example.mynavi;
+
+//（2）クラス宣言
+public class Hello {
+	// （3）メソッド宣言
+
+	public static void main(String[] args) {
+		// （4）文
+		final String GREETING_MSG = "こんにちは、世界！";
+		System.out.println(GREETING_MSG);
+
+	}
+
+}
