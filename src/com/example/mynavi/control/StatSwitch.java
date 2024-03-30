d3daaf3cfde4620ef61c4687f5b11309fbee30df
Takumi19980801 <takunomi1998@gmail.com>
2024-03-30 18:57:06 +0900

    switch命令

---
+++ src/com/example/mynavi/control/StatSwitch.java
@@ -0,0 +1,25 @@
+package com.example.mynavi.control;
+
+public class StatSwitch {
+
+	public static void main(String[] args) {
+		int val = 3;
+		switch (val) {
+		  case 1:
+		    System.out.println("変数valは1です");
+		    break;
+		  case 2:
+		    System.out.println("変数valは2です");
+		    break;
+		  case 3:
+		    System.out.println("変数valは3です");
+		    break;
+		  default:
+		    System.out.println("変数valは1、2、3いずれでもありません");
+		    break;
+		    
+		}  // 結果：変数valは3です
+
+	}
+
+}
