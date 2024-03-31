d8533b6fc50185eaaeb4a6a65ca4f9a0461ad639
Takumi19980801 <takunomi1998@gmail.com>
2024-03-31 17:42:38 +0900

    while命令

---
+++ src/com/example/mynavi/control/StatWhile.java
@@ -0,0 +1,14 @@
+package com.example.mynavi.control;
+
+public class StatWhile {
+
+	public static void main(String[] args) {
+		int val = 0;
+		while (val < 5) {
+			System.out.print(val + "/");
+			val++;
+		} //結果：0/1/2/3/4/
+
+	}
+
+}
