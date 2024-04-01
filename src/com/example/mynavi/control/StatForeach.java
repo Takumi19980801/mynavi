d4ea3ba7ed8cdb95e5fe026142a441decca6a799
Takumi19980801 <takunomi1998@gmail.com>
2024-04-01 12:50:33 +0900

    拡張for命令

---
+++ src/com/example/mynavi/control/StatForeach.java
@@ -0,0 +1,13 @@
+package com.example.mynavi.control;
+
+public class StatForeach {
+
+	public static void main(String[] args) {
+		String[] data = { "Java", "C#", "VB" };
+		for (String val : data) {
+			System.out.print(val + "/");
+		} // 結果：Java/C#/VB/
+
+	}
+
+}
