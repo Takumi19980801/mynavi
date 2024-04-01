008a405043cafea02fe4f78fae2bca95f6403194
Takumi19980801 <takunomi1998@gmail.com>
2024-04-01 12:32:02 +0900

    for命令

---
+++ src/com/example/mynavi/control/StatFor.java
@@ -0,0 +1,12 @@
+package com.example.mynavi.control;
+
+public class StatFor {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			System.out.print(i + "/");
+		}  // 結果；0/1/2/3/4/
+
+	}
+
+}
---
+++ src/com/example/mynavi/control/StatFor2.java
@@ -0,0 +1,12 @@
+package com.example.mynavi.control;
+
+public class StatFor2 {
+
+	public static void main(String[] args) {
+		for(int i = 0, j = 10; i < 5; i++, j++) {
+			System.out.println(i * j);
+		}
+
+	} // 結果：0、11、24、39、56
+
+}
