2f641d44df75de978b3c3f44fea36b4a394a5242
Takumi19980801 <takunomi1998@gmail.com>
2024-04-09 15:00:49 +0900

    import static命令

---
+++ src/com/example/mynavi/object/ClassStaticImport.java
@@ -0,0 +1,11 @@
+package com.example.mynavi.object;
+
+import static java.lang.Math.*;
+
+public class ClassStaticImport {
+
+	public static void main(String[] args) {
+		System.out.print(abs(-10)); // 結果：10
+	}
+
+}
