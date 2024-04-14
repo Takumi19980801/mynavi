b05c21bc8b835ba3100d88e245a38a701da78732
Takumi19980801 <takunomi1998@gmail.com>
2024-04-14 21:03:15 +0900

    ラムダ式を利用する

--- src/com/example/mynavi/object/AnonymousSample.java
+++ src/com/example/mynavi/object/AnonymousSample.java
@@ -3,11 +3,8 @@ package com.example.mynavi.object;
 public class AnonymousSample {
 
 	public static void main(String[] args) {
-		IPrintable p = new IPrintable() {
-			@Override
-			public void print() {
-				System.out.println("TEST");
-			}
+		IPrintable p = () -> {
+			System.out.println("TEST");
 		};
 		p.print(); // 結果：TEST
 	}
