aeae63576085063b89e7e35ed75ab524ac02fee4
Takumi19980801 <takunomi1998@gmail.com>
2024-06-03 21:22:46 +0900

    for文2

---
+++ JavaSilver(2)/mynavi2/For2.java
@@ -0,0 +1,18 @@
+package mynavi2;
+
+public class For2 {
+
+	public static void main(String[] args) {
+		int num = 50;
+		if (num % 2 == 1)
+			System.out.println("numは奇数です");
+		else
+			System.out.println("numは偶数です");
+		
+		System.out.println();
+		
+		for (int i = 1; i <= 3; i++) System.out.print(i);
+		System.out.println("繰り返し処理終了");
+	}
+
+}
