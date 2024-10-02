3fd381532bcaadcff7ddb810611aa4fbc845e332
Takumi19980801 <takunomi1998@gmail.com>
2024-10-02 15:43:14 +0900

    JavaGold8

---
+++ Java_Gold/mynavi/Java_Gold_1_9.java
@@ -0,0 +1,9 @@
+package mynavi;
+
+public interface Java_Gold_1_9 {
+	int number = 100;
+	public static String method1() {return "sa";};
+	//public static String method2();
+	public String method3();
+	public default String method4() {return "ss";};
+}
