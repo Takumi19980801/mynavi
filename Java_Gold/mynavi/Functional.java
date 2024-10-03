b94530c708bd890845434cbcb5a482229ca206ec
Takumi19980801 <takunomi1998@gmail.com>
2024-10-03 18:25:58 +0900

    JavaGold10

--- Java_Gold/mynavi/Functional.java
+++ Java_Gold/mynavi/Functional.java
@@ -2,6 +2,8 @@ package mynavi;
 
 @FunctionalInterface
 public interface Functional<T> {
+	/*関数型インターフェースの要件として、１つの抽象メソッドを持つ必要がある
+	 equalsはデフォルトメソッドのため、抽象メソッドにカウントされない*/
 	boolean test(T t);
-	boolean equals(Object obj);
+	//boolean equals(Object obj);
 }
